f2cf670a00a01c362e0d10fab5f9320e24568ae7
suyash-varade12 <108572327+suyash-varade12@users.noreply.github.com>
2025-08-08 18:37:06 +0530

    Update deployApproval.groovy

--- vars/deployApproval.groovy
+++ vars/deployApproval.groovy
@@ -1,1 +1,17 @@
-
+def call() {
+    stage('Deploy approval') {
+        when {
+            allOf {
+                expression { params.deployToProd == true }
+                expression { currentBuild.result == null }
+            }
+        }
+        steps {
+            script {
+                timeout(time: 7, unit: 'DAYS') {
+                    input message: "Deploy to production? Press 'Proceed' to continue or 'Abort' to stop."
+                }
+            }
+        }
+    }
+}
